0297137574143f84220fdbbffab02072e62eb93e
philip-morlier <philipmorlier@gmail.com>
2021-08-19 07:01:57 -0700

    further development of funtions

--- plugins/packages/isSynced/main.go
+++ plugins/packages/isSynced/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/plugins"
@@ -13,38 +13,62 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
-type MyService struct {
+type IsSyncedService struct {
 	backend interfaces.Backend
 	stack   *node.Node
+}
+
+var HTTPApiFlag = cli.StringFlag{
+	Name:  "isSynced",
+	Usage: "API's offered over the HTTP-RPC interface",
+	Value: "",
 }
 
 var pl *plugins.PluginLoader
 var cache *lru.Cache
 
-//The initialize method needs to be modified in order to allow geth to start without appending flags
-
+//This still doesnt work I assume is has to do with some funtion not being called correctly
+//as far as I can tell the name is registered but there is a warning logged saying the method is unavailable
 func Initialize(ctx *cli.Context, loader *plugins.PluginLoader) {
 	pl = loader
-
-	cache, _ = lru.New(128) // TODO: Make size configurable
-	if !ctx.GlobalBool(utils.SnapshotFlag.Name) {
-		log.Warn("Snapshots are required for StateUpdate plugins, but are currently disabled. State Updates will be unavailable")
+	cache, _ = lru.New(128)
+	if ctx.GlobalString(name) == "" {
+		ctx.GlobalSet("", HTTPApiFlag.Name)
+		log.Info("name is actually set")
 	}
-	log.Info("loaded isSynced plugin")
+	//I added these and other log statements to try and track down the issue
+	log.Info("default message")
 }
-
 func GetAPIs(stack *node.Node, backend interfaces.Backend) []rpc.API {
 	return []rpc.API{
 		{
-			Namespace: "mynamespace",
+			Namespace: "plugeth",
 			Version:   "1.0",
-			Service:   &MyService{backend, stack},
+			Service:   &IsSyncedService{backend, stack},
 			Public:    true,
 		},
 	}
 }
 
-func (myserv *MyService) IsSynced(ctx context.Context) bool {
-	dwnlder := myserv.backend.Downloader()
-	return myserv.stack.Server().PeerCount() > 0 && dwnlder.Progress().CurrentBlock >= dwnlder.Progress().HighestBlock
+var peers bool
+var synced bool
+
+func (service *IsSyncedService) IsSynced(ctx context.Context) interface{} {
+	progress := service.backend.Downloader().Progress()
+	peercount := service.stack.Server().PeerCount()
+	if peercount > 0 {
+		peers = true
+	}
+	if progress.CurrentBlock >= progress.HighestBlock {
+		synced = true
+	}
+	return map[string]interface{}{
+		"startingBlock": hexutil.Uint64(progress.StartingBlock),
+		"currentBlock":  hexutil.Uint64(progress.CurrentBlock),
+		"highestBlock":  hexutil.Uint64(progress.HighestBlock),
+		"pulledStates":  hexutil.Uint64(progress.PulledStates),
+		"knownStates":   hexutil.Uint64(progress.KnownStates),
+		"activePeers":   peers,
+		"nodeIsSynced":  synced,
+	}
 }
