79a3747619401b61cf74b7aa44b6eaff43a60802
philip-morlier <philipmorlier@gmail.com>
2021-08-19 10:26:08 -0700

    Fine tuning methods and structuring

--- plugins/packages/isSynced/main.go
+++ plugins/packages/isSynced/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 
+	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/plugins"
 	"github.com/ethereum/go-ethereum/plugins/interfaces"
 	"github.com/ethereum/go-ethereum/rpc"
-	lru "github.com/hashicorp/golang-lru"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -19,26 +19,21 @@ type IsSyncedService struct {
 }
 
 var HTTPApiFlag = cli.StringFlag{
-	Name:  "isSynced",
+	Name:  "http.api",
 	Usage: "API's offered over the HTTP-RPC interface",
 	Value: "",
 }
 
-var pl *plugins.PluginLoader
-var cache *lru.Cache
-
-//This still doesnt work I assume is has to do with some funtion not being called correctly
-//as far as I can tell the name is registered but there is a warning logged saying the method is unavailable
 func Initialize(ctx *cli.Context, loader *plugins.PluginLoader) {
-	pl = loader
-	cache, _ = lru.New(128)
-	if ctx.GlobalString(name) == "" {
-		ctx.GlobalSet("", HTTPApiFlag.Name)
-		log.Info("name is actually set")
+	v := ctx.GlobalString(utils.HTTPApiFlag.Name)
+	if v != "" {
+		ctx.GlobalSet(HTTPApiFlag.Name, v+"plugeth")
+	} else {
+		ctx.GlobalSet(HTTPApiFlag.Name, "eth,net,web3,plugeth")
+		log.Info("Loaded isSynced plugin")
 	}
-	//I added these and other log statements to try and track down the issue
-	log.Info("default message")
 }
+
 func GetAPIs(stack *node.Node, backend interfaces.Backend) []rpc.API {
 	return []rpc.API{
 		{
@@ -50,25 +45,16 @@ func GetAPIs(stack *node.Node, backend interfaces.Backend) []rpc.API {
 	}
 }
 
-var peers bool
-var synced bool
-
 func (service *IsSyncedService) IsSynced(ctx context.Context) interface{} {
 	progress := service.backend.Downloader().Progress()
 	peercount := service.stack.Server().PeerCount()
-	if peercount > 0 {
-		peers = true
-	}
-	if progress.CurrentBlock >= progress.HighestBlock {
-		synced = true
-	}
 	return map[string]interface{}{
 		"startingBlock": hexutil.Uint64(progress.StartingBlock),
 		"currentBlock":  hexutil.Uint64(progress.CurrentBlock),
 		"highestBlock":  hexutil.Uint64(progress.HighestBlock),
 		"pulledStates":  hexutil.Uint64(progress.PulledStates),
 		"knownStates":   hexutil.Uint64(progress.KnownStates),
-		"activePeers":   peers,
-		"nodeIsSynced":  synced,
+		"activePeers":   peercount > 0,
+		"nodeIsSynced":  progress.CurrentBlock >= progress.HighestBlock,
 	}
 }
