e87859e9ab3d166024e3f8336e9b7447ab5daba9
philip-morlier <philipmorlier@gmail.com>
2022-05-16 09:48:46 -0700

    Updating mod and sum to reflect plugeth-utils v0.0.17. Made corresponding changes to wrapper function to reflect the close() method on the Node object returned by NewNode(). Created a new tag v1.10.17-2 to reflect these changes.

--- plugins/wrappers/wrappers.go
+++ plugins/wrappers/wrappers.go
@@ -217,6 +217,9 @@ func (n *Node) ResolvePath(x string) string {
 func (n *Node) Attach() (core.Client, error) {
 	return n.n.Attach()
 }
+func (n *Node) Close() error {
+	return n.n.Close()
+}
 
 // type WrappedBlockContext struct {
 // 	b vm.BlockContext
