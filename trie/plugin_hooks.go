34191cb50a5f2329007dad9bdd73fb24f57086ca
philip-morlier <philipmorlier@gmail.com>
2023-03-08 14:51:21 -0800

    Removed unnecessary comments from trie/plugin_hooks.go

--- trie/plugin_hooks.go
+++ trie/plugin_hooks.go
@@ -46,23 +46,3 @@ func pluginPostTrieCommit(node common.Hash) {
 	}
 	PluginPostTrieCommit(plugins.DefaultPluginLoader, node)
 }
-
-// func PluginGetRPCCalls(pl *plugins.PluginLoader, id, method, params string) {
-// 	fnList := pl.Lookup("GetRPCCalls", func(item interface{}) bool {
-// 		_, ok := item.(func(string, string, string))
-// 		return ok
-// 	})
-// 	for _, fni := range fnList {
-// 		if fn, ok := fni.(func(string, string, string)); ok {
-// 			fn(id, method, params)
-// 		}
-// 	}
-// }
-
-// func pluginGetRPCCalls(id, method, params string) {
-// 	if plugins.DefaultPluginLoader == nil {
-// 		log.Warn("Attempting GerRPCCalls, but default PluginLoader has not been initialized")
-// 		return
-// 	}
-// 	PluginGetRPCCalls(plugins.DefaultPluginLoader, id, method, params)
-// }
