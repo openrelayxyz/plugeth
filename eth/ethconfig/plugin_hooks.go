7f1b5c5f9b36a50be6e4529787342f12c17c11d1
philip-morlier <philipmorlier@gmail.com>
2023-03-24 16:05:39 -0700

    Progress made on 3/24/23

--- eth/ethconfig/plugin_hooks.go
+++ eth/ethconfig/plugin_hooks.go
@@ -2,32 +2,18 @@ package ethconfig
 
 import (
 	"github.com/ethereum/go-ethereum/log"
-	// "github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/plugins"
 	"github.com/ethereum/go-ethereum/plugins/wrappers"
 	wengine "github.com/ethereum/go-ethereum/plugins/wrappers/engine"
 	"github.com/ethereum/go-ethereum/plugins/wrappers/backendwrapper"
-	// "github.com/ethereum/go-ethereum/rpc"
 	"github.com/openrelayxyz/plugeth-utils/core"
 	"github.com/openrelayxyz/plugeth-utils/restricted"
 
-	// "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
-	// "github.com/ethereum/go-ethereum/consensus/beacon"
-	// "github.com/ethereum/go-ethereum/consensus/clique"
-	// "github.com/ethereum/go-ethereum/consensus/ethash"
-	// "github.com/ethereum/go-ethereum/core"
-	// "github.com/ethereum/go-ethereum/core/txpool"
-	// "github.com/ethereum/go-ethereum/eth/downloader"
-	// "github.com/ethereum/go-ethereum/eth/gasprice"
 	"github.com/ethereum/go-ethereum/ethdb"
-	// "github.com/ethereum/go-ethereum/log"
-	// "github.com/ethereum/go-ethereum/miner"
 	"github.com/ethereum/go-ethereum/node"
-	// "github.com/ethereum/go-ethereum/params"
 
 	pconsensus "github.com/openrelayxyz/plugeth-utils/restricted/consensus"
-	// pparams "github.com/openrelayxyz/plugeth-utils/restricted/params"
 )
 
 
